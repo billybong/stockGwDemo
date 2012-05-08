40e43862918359da89cc0b32010a2eda719b70da
Billy Sjöberg <billy.sjoberg@gmail.com>
2012-05-08 11:06:44 +0200

    Added idempotent consumer for polling route

--- tickergateway/src/main/java/se/rl/tickergateway/groovy/TickerTechRoute.groovy
+++ tickergateway/src/main/java/se/rl/tickergateway/groovy/TickerTechRoute.groovy
@@ -5,16 +5,19 @@ import groovy.xml.MarkupBuilder
 
 import org.apache.camel.Expression
 import org.apache.camel.language.groovy.GroovyRouteBuilder
+import org.apache.camel.processor.idempotent.MemoryIdempotentRepository;
 
 class TickerTechRoute extends GroovyRouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
-		from("quartz://callTickerTech?cron=0/30+*+*+*+*+?").autoStartup(false).routeId("TickerTechGateway")
+		from("quartz://callTickerTech?cron=0/10+*+*+*+*+?").autoStartup(false).routeId("TickerTechGateway")
 			.enrich("http://localhost/tickertech.json")
-			.convertBodyTo(String.class)
+			.convertBodyTo(String.class) //Required as http endpoint returns non cacheable InputStream
 			.split().method(FeedSplitter.class, "split")
-				.log("Incoming ticker from TickerTech \${body}")
+				.log('Incoming ticker from TickerTech ${body}')
+				.idempotentConsumer(simple('${body[symbol]}:${body[timestamp]}'), MemoryIdempotentRepository.memoryIdempotentRepository()) //Key = i.e. IBM:2012-05-06:...
+				.log('Ticker ${body[symbol]}:${body[timestamp]} passed idempotency test')
 				.setHeader("from").constant('TickerTech')
 				.bean(Transformer, "transform")
 				.to("activemq:queue:ticker")
